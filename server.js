0409e5d8d75ac7ed559edf190fc46ddfd38057ce
Abd Alrhman <abdalrhman@Abds-MacBook-Pro.local>
2020-11-19 14:17:01 +0200

    lab 05

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { request, response } = require('express');
 // declare a variable to use an express library.
 const express = require('express');
 
@@ -7,8 +8,25 @@ const express = require('express');
 const server = express();
 
 // declare a variable that store a hostlocal number.
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+server.use(express.static('./public'));
 
 server.get('/data', (request, response) => {
     response.send('Hello There!');
+});
+
+server.get('/jobs', (request, response) => {
+    let jobs = [
+        {title : 'Full-stack developer'},
+        {title: 'Process Engineer'},
+        {title: 'Software Engineer'},
+        {title: 'Author'},
+        {title: 'Translator'}
+    ];
+    response.send(jobs);
+});
+
+server.listen(PORT, ()=>{
+    console.log('listening');
 });
